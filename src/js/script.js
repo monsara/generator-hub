ab37ee139e0187b670fac03813bd4a68e444946f
Viktor Hirenko <viktor.hirenko@gmail.com>
2019-12-27 16:27:38 +0200

    add tabs component

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,56 @@
-// собираем все якоря; устанавливаем время анимации и количество кадров
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-  // animationTime = 300,
-  animationTime = 7000,
-  // framesCount = 20;
-  framesCount = 1200;
-
-anchors.forEach(function(item) {
-  // каждому якорю присваиваем обработчик события
-  item.addEventListener('click', function(e) {
-    // убираем стандартное поведение
-    e.preventDefault();
-
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-    let coordY =
-      document.querySelector(item.getAttribute('href')).getBoundingClientRect()
-        .top + window.pageYOffset;
-
-    // запускаем интервал, в котором
-    let scroller = setInterval(function() {
-      // считаем на сколько скроллить за 1 такт
-      let scrollBy = coordY / framesCount;
-
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
-      if (
-        scrollBy > window.pageYOffset - coordY &&
-        window.innerHeight + window.pageYOffset < document.body.offsetHeight
-      ) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
-        window.scrollBy(0, scrollBy);
-      } else {
-        // иначе добираемся до элемента и выходим из интервала
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-      // время интервала равняется частному от времени анимации и к-ва кадров
-    }, animationTime / framesCount);
-  });
-});
-
-// =============================================================================
-
 document.addEventListener('DOMContentLoaded', function() {
+  /*
+   * Animation scroll
+   **/
+
+  // собираем все якоря; устанавливаем время анимации и количество кадров
+  const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+    // animationTime = 300,
+    animationTime = 7000,
+    // framesCount = 20;
+    framesCount = 1200;
+
+  anchors.forEach(function(item) {
+    // каждому якорю присваиваем обработчик события
+    item.addEventListener('click', function(e) {
+      // убираем стандартное поведение
+      e.preventDefault();
+
+      // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+      let coordY =
+        document
+          .querySelector(item.getAttribute('href'))
+          .getBoundingClientRect().top + window.pageYOffset;
+
+      // запускаем интервал, в котором
+      let scroller = setInterval(function() {
+        // считаем на сколько скроллить за 1 такт
+        let scrollBy = coordY / framesCount;
+
+        // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+        // и дно страницы не достигнуто
+        if (
+          scrollBy > window.pageYOffset - coordY &&
+          window.innerHeight + window.pageYOffset < document.body.offsetHeight
+        ) {
+          // то скроллим на к-во пикселей, которое соответствует одному такту
+          window.scrollBy(0, scrollBy);
+        } else {
+          // иначе добираемся до элемента и выходим из интервала
+          window.scrollTo(0, coordY);
+          clearInterval(scroller);
+        }
+        // время интервала равняется частному от времени анимации и к-ва кадров
+      }, animationTime / framesCount);
+    });
+  });
+
+  /*
+   * Open modal
+   **/
+
   const page = document.querySelector('.page');
   const orderForm = document.querySelector('.order-form');
-
   const openModalBtns = document.querySelectorAll(
     '.button[data-action="open-modal"]',
   );
@@ -92,23 +98,40 @@ document.addEventListener('DOMContentLoaded', function() {
     if (key !== 'Escape') return;
 
     handleCloseModal();
+  }
 
-    console.log('Работает', evt);
+  /*
+   * Toggle tab buttons and tab panels
+   **/
+
+  const tabs = document.querySelector('.tabs');
+  const tabsPanels = tabs.querySelectorAll('.tabs__panel');
+  const tabsBtns = tabs.querySelectorAll('.tabs__button');
+
+  tabsBtns.forEach(tabsBtn => {
+    tabsBtn.addEventListener('click', handleTabsBtnsClick);
+  });
+
+  function handleTabsBtnsClick() {
+    toggleButtons.call(this);
+    toggleTabs.call(this);
+  }
+
+  function toggleButtons() {
+    //Remove button_checked class from all buttons
+    tabsBtns.forEach(button => button.classList.remove('button_checked'));
+    //Add button_checked class to current button
+    this.classList.add('button_checked');
+  }
+
+  function toggleTabs() {
+    //Remove tabs__panel_active class from all tabs
+    tabsPanels.forEach(tab => tab.classList.remove('tabs__panel_active'));
+    //Find linked tab by date-tab attribute and add tabs__panel_active' class
+    tabs
+      .querySelector('.' + this.dataset.tab)
+      .classList.add('tabs__panel_active');
+
+    this.style.display = 'block';
   }
 });
-
-// =======
-
-// const select = document.querySelector('.select_test');
-// const popup = document.querySelector('.popup_test');
-// const btn = document.querySelector('.select .button_test');
-
-// console.log(btn);
-
-// select.onmouseenter = function() {
-//   popup.classList.add('popup_visible');
-// };
-
-// select.onmouseleave = function() {
-//   popup.classList.remove('popup_visible');
-// };
